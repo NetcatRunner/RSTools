5ff2af3585c6c4b4183a5807263c36d4729c83ca
NetCatRunner <romain.marcot@epitech.eu>
2026-08-15 16:01:45 +0200

    [UPDATE] - Update logger now accept reference or pointer in macro and callback sink has 2 types callback

--- include/RST/log/Log.hpp
+++ include/RST/log/Log.hpp
@@ -18,17 +18,21 @@
 
 namespace RST::Log {
 
+    namespace detail {
+        [[nodiscard]] inline Logger& deref(Logger& logger) noexcept { return logger; }
+        [[nodiscard]] inline Logger& deref(Logger* logger) noexcept { return *logger; }
 
-#define RST_LOG(lvl, ...) RST::Log::Registry::defaultLogger()->log((lvl), RST_SOURCE_LOCATION, __VA_ARGS__)
+        template <typename Ptr> [[nodiscard]] Logger& deref(const Ptr& pointer) { return *pointer; }
+    }
+}
 
-#define LOG_TRACE(...)  RST_LOG(RST::Log::LogLevel::Trace, __VA_ARGS__)
-#define LOG_DEBUG(...)  RST_LOG(RST::Log::LogLevel::Debug, __VA_ARGS__)
-#define LOG_INFO(...)   RST_LOG(RST::Log::LogLevel::Info,  __VA_ARGS__)
-#define LOG_WARN(...)   RST_LOG(RST::Log::LogLevel::Warn,  __VA_ARGS__)
-#define LOG_ERROR(...)  RST_LOG(RST::Log::LogLevel::Error, __VA_ARGS__)
-#define LOG_FATAL(...)  RST_LOG(RST::Log::LogLevel::Fatal, __VA_ARGS__)
-
-#define RST_LOG_L(logger, lvl, ...) (logger)->log((lvl), RST_SOURCE_LOCATION, __VA_ARGS__)
+#define RST_LOG_L(logger, lvl, ...)                                            \
+    do {                                                                       \
+        ::RST::Log::Logger& target = ::RST::Log::detail::deref(logger);        \
+        if (target.shouldLog(lvl)) {                                           \
+            target.log((lvl), RST_SOURCE_LOCATION, __VA_ARGS__);               \
+        }                                                                      \
+    } while (false)
 
 #define LOG_TRACE_L(logger, ...)  RST_LOG_L(logger, RST::Log::LogLevel::Trace, __VA_ARGS__)
 #define LOG_DEBUG_L(logger, ...)  RST_LOG_L(logger, RST::Log::LogLevel::Debug, __VA_ARGS__)
@@ -36,4 +40,12 @@ namespace RST::Log {
 #define LOG_WARN_L(logger, ...)   RST_LOG_L(logger, RST::Log::LogLevel::Warn,  __VA_ARGS__)
 #define LOG_ERROR_L(logger, ...)  RST_LOG_L(logger, RST::Log::LogLevel::Error, __VA_ARGS__)
 #define LOG_FATAL_L(logger, ...)  RST_LOG_L(logger, RST::Log::LogLevel::Fatal, __VA_ARGS__)
-}
+
+#define RST_LOG(lvl, ...) RST_LOG_L(::RST::Log::Registry::defaultLogger(), (lvl), __VA_ARGS__)
+
+#define LOG_TRACE(...)  RST_LOG(RST::Log::LogLevel::Trace, __VA_ARGS__)
+#define LOG_DEBUG(...)  RST_LOG(RST::Log::LogLevel::Debug, __VA_ARGS__)
+#define LOG_INFO(...)   RST_LOG(RST::Log::LogLevel::Info,  __VA_ARGS__)
+#define LOG_WARN(...)   RST_LOG(RST::Log::LogLevel::Warn,  __VA_ARGS__)
+#define LOG_ERROR(...)  RST_LOG(RST::Log::LogLevel::Error, __VA_ARGS__)
+#define LOG_FATAL(...)  RST_LOG(RST::Log::LogLevel::Fatal, __VA_ARGS__)
--- include/RST/log/Sinks/CallbackSink.hpp
+++ include/RST/log/Sinks/CallbackSink.hpp
@@ -1,27 +1,51 @@
 #pragma once
 #include "ASink.hpp"
+
 #include <functional>
+#include <string_view>
+#include <utility>
 
 namespace RST::Log {
 
-class CallbackSink : public ASink {
-public:
-    using Callback = std::function<void(const LogMessage&)>;
+    class CallbackSink : public ASink {
+    public:
+        using MessageCallback = std::function<void(const LogMessage&)>;
+        using LineCallback = std::function<void(LogLevel, std::string_view)>;
 
-    CallbackSink(Callback cb): _callback(std::move(cb)){}
+        CallbackSink() = default;
+        explicit CallbackSink(MessageCallback cb) : _messageCallback(std::move(cb)) {}
+        explicit CallbackSink(LineCallback cb) : _lineCallback(std::move(cb)) {}
 
-    void setCallback(Callback cb) {
-        _callback = std::move(cb);
-    }
+        void setCallback(MessageCallback cb) {
+            _messageCallback = std::move(cb);
+            _lineCallback = nullptr;
+        }
 
-protected:
-    void log(const LogMessage& msg) override {
-        if (_callback)
-            _callback(msg);
-    }
+        void setCallback(LineCallback cb) {
+            _lineCallback = std::move(cb);
+            _messageCallback = nullptr;
+        }
 
-private:
-    Callback _callback = nullptr;
-};
+        void clearCallback() noexcept {
+            _messageCallback = nullptr;
+            _lineCallback = nullptr;
+        }
 
-} // namespace RST::Log
+        [[nodiscard]] bool hasCallback() const noexcept { return static_cast<bool>(_messageCallback) || static_cast<bool>(_lineCallback); }
+
+    protected:
+        void log(const LogMessage& msg) override {
+            if (_messageCallback) {
+                _messageCallback(msg);
+            }
+            if (_lineCallback) {
+                _lineCallback(msg.level, formatted(msg));
+            }
+        }
+
+    private:
+        MessageCallback _messageCallback = nullptr;
+        LineCallback _lineCallback = nullptr;
+    };
+
+}
